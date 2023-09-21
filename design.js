4852b6ae36a099a25090d2ece2195f9c4afded2c
Wong Jun Onn <145476852+TARUMT-JunOnn@users.noreply.github.com>
2023-09-21 14:32:43 +0800

    Co-authored-by: tarumt-Jiaye <tarumt-Jiaye@users.noreply.github.com> Co-authored-by: zhixuanyeeeeeee <zhixuanyeeeeeee@users.noreply.github.com>

--- design.js
+++ design.js
@@ -5,19 +5,21 @@ return parts.at(-1);
 
 let web = getLastPart(document.URL)
 console.log(web)
-if (web=="index.html")
+if (web=="home.html")
 {
-    document.getElementById("home").style.backgroundColor = "#dddddd"
+    document.getElementById("home").style.backgroundColor = "rgba(237,233,225,1)"
+    //document.getElementById("cartlink").style.display= "block"
 }
 else if (web=="product.html")
 {
-    document.getElementById("product").style.backgroundColor = "#dddddd"
+    document.getElementById("product").style.backgroundColor = "rgba(237,233,225,1)"
 }
 else if (web=="about-us.html")
 {
-    document.getElementById("aboutus").style.backgroundColor = "#dddddd"
+    document.getElementById("aboutus").style.backgroundColor = "rgba(237,233,225,1)"
 }
 else if (web=="login.html")
 {
-    document.getElementById("login").style.backgroundColor = "#dddddd"
+    document.getElementById("login").style.backgroundColor = "rgba(237,233,225,1)"
 }
+
