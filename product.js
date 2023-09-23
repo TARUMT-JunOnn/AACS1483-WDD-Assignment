51fc568c750e5ebde64f62fb93c4c3e1d1c7c91f
Wong Jun Onn <145476852+TARUMT-JunOnn@users.noreply.github.com>
2023-09-23 23:55:22 +0800

    update

--- product.js
+++ product.js
@@ -1,11 +1,24 @@
 let num = 6
 let product = [0,0,0,0,0,0]
+const plus1txt = document.createTextNode("+1")
+const plus1 = document.createElement("p")
+const createclass = document.createAttribute("class")
+createclass.value = "plus1"
+plus1.setAttributeNode(createclass)
+plus1.appendChild(plus1txt)
+
+console.log(plus1)
+
 
 let x = document.getElementsByClassName("cartImgproduct")
 for (let i =0; i < x.length ; i++){
     x[i].addEventListener('click', function cart(){
         product[i]++
         console.log(i +" "+product[i])
+        x[i].parentNode.appendChild(plus1)
+        const clear = document.getElementsByClassName("plus1")
+        setTimeout(clear.remove(), 3000)
+        
     })
 }
 
@@ -21,16 +34,7 @@ function display(){
     document.getElementById("countProduct").innerHTML = num + " Product"
 }
 
-document.addEventListener('DOMContentLoaded', function display(){
-    document.getElementById("product1").style.display= "block"
-    document.getElementById("product2").style.display= "block"
-    document.getElementById("product3").style.display= "block"
-    document.getElementById("product4").style.display= "block"
-    document.getElementById("product5").style.display= "block"
-    document.getElementById("product6").style.display= "block"
-    num = 6
-    document.getElementById("countProduct").innerHTML = num + " Product"
-});
+document.addEventListener('DOMContentLoaded', display());
 
 function bag(){
     document.getElementById("product1").style.display= "block"
