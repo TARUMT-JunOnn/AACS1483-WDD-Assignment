d171b773fde7a8aaef35f5bb073a14e07f1d941a
Wong Jun Onn <145476852+TARUMT-JunOnn@users.noreply.github.com>
2023-09-23 11:18:40 +0800

    📃 docs:

--- product.js
+++ product.js
@@ -1,33 +1,56 @@
-let num = 3
+let num = 6
 
 function display(){
     document.getElementById("product1").style.display= "block"
     document.getElementById("product2").style.display= "block"
     document.getElementById("product3").style.display= "block"
-    num = 3
-    document.getElementById("countProduct").textContent = num + " Product"
+    document.getElementById("product4").style.display= "block"
+    document.getElementById("product5").style.display= "block"
+    document.getElementById("product6").style.display= "block"
+    num = 6
+    document.getElementById("countProduct").innerHTML = num + " Product"
 }
 
 document.addEventListener('DOMContentLoaded', function display(){
     document.getElementById("product1").style.display= "block"
     document.getElementById("product2").style.display= "block"
     document.getElementById("product3").style.display= "block"
-    num = 3
-    document.getElementById("countProduct").textContent = num + " Product"
+    document.getElementById("product4").style.display= "block"
+    document.getElementById("product5").style.display= "block"
+    document.getElementById("product6").style.display= "block"
+    num = 6
+    document.getElementById("countProduct").innerHTML = num + " Product"
 });
 
 function bag(){
     document.getElementById("product1").style.display= "block"
-    document.getElementById("product2").style.display= "none"
+    document.getElementById("product2").style.display= "block"
     document.getElementById("product3").style.display= "none"
-    num = 1
+    document.getElementById("product4").style.display= "none"
+    document.getElementById("product5").style.display= "none"
+    document.getElementById("product6").style.display= "none"
+    num = 2
     document.getElementById("countProduct").textContent = num + " Product"
 }
 
 function earing(){
     document.getElementById("product1").style.display= "none"
-    document.getElementById("product2").style.display= "block"
+    document.getElementById("product2").style.display= "none"
     document.getElementById("product3").style.display= "block"
+    document.getElementById("product4").style.display= "block"
+    document.getElementById("product5").style.display= "none"
+    document.getElementById("product6").style.display= "none"
+    num = 2
+    document.getElementById("countProduct").textContent = num + " Product"
+}
+
+function neck(){
+    document.getElementById("product1").style.display= "none"
+    document.getElementById("product2").style.display= "none"
+    document.getElementById("product3").style.display= "none"
+    document.getElementById("product4").style.display= "none"
+    document.getElementById("product5").style.display= "block"
+    document.getElementById("product6").style.display= "block"
     num = 2
     document.getElementById("countProduct").textContent = num + " Product"
 }
